f7f3a6ffe29673d4720cd52d53c3d54fcd9881fe
thabzzzzz <tmosenohi99@gmail.com>
2023-09-09 17:44:50 +0200

    First commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+ 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+ 
+Route::get('/', [ProductsController::class, 'index']);
+Route::get('cart', [ProductsController::class, 'cart'])->name('cart');
+Route::get('add-to-cart/{id}', [ProductsController::class, 'addToCart'])->name('add_to_cart');
+Route::patch('update-cart', [ProductsController::class, 'update'])->name('update_cart');
+Route::delete('remove-from-cart', [ProductsController::class, 'remove'])->name('remove_from_cart');
